dad339907865c9ff5306783f19de89c2f87b7121
hk2k1 <86213550+hk2k1@users.noreply.github.com>
2024-05-17 08:31:38 +0800

    Updated page.tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     root: true,
     extends: ['plugin:@next/next/recommended', '@payloadcms', 'prettier'],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+    },
     ignorePatterns: ['**/payload-types.ts'],
     plugins: ['prettier'],
 };
